3c10253bd838ca5e2b7281f7de7fb261c3761793
marianacezarm <mariana.cezarm@gmail.com>
2026-05-30 00:26:09 -0300

    chore: install dotenv dependency and use .env

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -4,7 +4,9 @@ export class BasePage {
     constructor(protected page: Page) {}
 
     async navigate(path:string = ''){
-        const baseUrl = process.env.BASE_URL || 'https://www.saucedemo.com/';
+        const baseUrl = 
+            process.env.BASE_URL || 'https://www.saucedemo.com/';
+        
         await this.page.goto(`${baseUrl}${path}`);
     }
 }
